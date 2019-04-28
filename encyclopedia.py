8a985a461919d30750099bfb9d156df149379a9b
cr00z <netrebinr@gmail.com>
2019-04-28 16:21:30 +0700

    fix some bugs in md2html convert

--- encyclopedia.py
+++ encyclopedia.py
@@ -17,14 +17,18 @@ def load_configuration(json_filename):
         return
 
 
+def convert_filename(md_filename):
+    return md_filename.replace('.md', '.html').replace(' ', '_').replace('&', '')
+
+
 def convert_config_to_artdict(config):
     articles = dict()
     for topic in config['topics']:
         articles[topic['slug']] = {'title': topic['title'], 'articles': [] }
     for article in config['articles']:
         articles[article['topic']]['articles'].append({
-            'title': article['title'],
-            'source': 'articles/' + article['source'].replace('.md', '.html')
+            'title': html.escape(article['title']),
+            'source': 'articles/' + convert_filename(article['source'])
         })
     return articles
 
@@ -51,6 +55,18 @@ def create_article(jinja2_env, article_name, article_html):
     return jinja2_template.render(name=article_name, content=article_html)
 
 
+def fix_some_translation_bugs(input_html):
+    out_html = input_html.replace(
+        '<pre><code>:::python',
+        '<div class="card"><div class="card-body"><pre>'
+    )
+    out_html = out_html.replace(
+        '<pre><code>:::cpp',
+        '<div class="card"><div class="card-body"><pre>'
+    )
+    return out_html.replace('</code></pre>', '</pre></div></div>')
+
+
 if __name__ == '__main__':
     jinja2_env = Environment(loader=FileSystemLoader('templates'))
     config = load_configuration('config.json')
@@ -59,16 +75,14 @@ if __name__ == '__main__':
         create_index(jinja2_env, convert_config_to_artdict(config))
     )
     for article in config['articles']:
-        md_content = html.escape(
-            load_markdown(os.path.join('articles', article['source']))
-        )
-        article_html = markdown.markdown(md_content)
+        md_content = load_markdown(os.path.join('articles', article['source']))
+        article_html = fix_some_translation_bugs(markdown.markdown(md_content))
         md_article_file = os.path.basename(article['source'])
         save_page(
             os.path.join(
                 HTML_FOLDER,
                 ARTS_FOLDER,
-                article['source'].replace('.md', '.html')
+                convert_filename(article['source'])
             ),
             create_article(jinja2_env, html.escape(article['title']), article_html)
         )
